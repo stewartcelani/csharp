f654406c5e03b8e51df41ab357e1ea26c9de80e8
Stewart Celani <me@stewartcelani.com>
2022-04-21 17:48:09 +0930

    Added Template

---
+++ Template/Program.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Bogus;
+
+namespace Template
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchy>();
+        }
+    }
+
+    [MemoryDiagnoser()]
+    public class Benchy
+    {
+        public Benchy()
+        {
+            // Any Setup
+            SetupForBenchmark();
+        }
+
+        private void SetupForBenchmark()
+        {
+           var faker = new Faker("en");
+
+        }
+        
+        [Benchmark]
+        public void Bechmark1()
+        {
+            
+        }
+
+        [Benchmark]
+        public void Bechmark2()
+        {
+            
+        }
+    }
+    
+}
