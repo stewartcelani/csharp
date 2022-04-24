53ad29db6fa78834ff48e1622bbe6c69ce0b5a60
Stewart Celani <me@stewartcelani.com>
2022-04-24 23:05:22 +0930

    Sync vs Parallel loop downloading benchmark. Parallel was 45% faster.

---
+++ Benchmarks.Download.SynchronousVsParallel/Benchmark.cs
@@ -0,0 +1,66 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.VisualStudio.TestPlatform.CrossPlatEngine.TestRunAttachmentsProcessing;
+
+namespace Benchmarks.Download.SynchronousVsParallel;
+
+[MemoryDiagnoser()]
+public class Benchmark
+{
+    private readonly HttpClient _http = new ();
+    private readonly string _downloadPath = @"C:\inetpub\wwwroot\wwwroot\temp\downloadto";
+    private readonly string _baseDownloadUrl = "https://localhost/temp/downloadfrom";
+    private readonly List<File> _files = new();
+
+    public Benchmark()
+    {
+        if (!Directory.Exists(_downloadPath))
+        {
+            Directory.CreateDirectory(_downloadPath);
+        }
+
+        for (var i = 1; i <= 1000; i++)
+        {
+            _files.Add(new File()
+            {
+                DownloadPath = @$"{_downloadPath}\{i}.tmp",
+                DownloadUrl = @$"{_baseDownloadUrl}/{i}.tmp"
+            });
+        }
+    }
+    
+    [Benchmark]
+    public async Task SynchronousLoop()
+    {
+        foreach (File file in _files)
+        {
+            await using Stream stream = await _http.GetStreamAsync(file.DownloadUrl);
+            await using FileStream fileStream = new (file.DownloadPath, FileMode.Create);
+            await stream.CopyToAsync(fileStream);
+            //Console.WriteLine($"+ {file.DownloadPath}");
+        }
+    }
+
+    [Benchmark]
+    public async Task ParallelLoop()
+    {
+        var parallelOptions = new ParallelOptions()
+        {
+            MaxDegreeOfParallelism = 10
+        };
+        await Parallel.ForEachAsync(_files, parallelOptions, async (file, ct) =>
+        {
+            await using Stream stream = await _http.GetStreamAsync(file.DownloadUrl, ct);
+            await using FileStream fileStream = new(file.DownloadPath, FileMode.Create);
+            await stream.CopyToAsync(fileStream, ct);
+            //Console.WriteLine($"+ {file.DownloadPath}");
+        });
+    }
+    
+}
+
+public record File
+{
+    public string DownloadUrl { get; set; }
+    public string DownloadPath { get; set; }
+}
---
+++ Benchmarks.Download.SynchronousVsParallel/README.md
@@ -0,0 +1,8 @@
+﻿Downloading 1000 randomly generated files between 0 and 5mb in size.
+
+Parallel loop 45% faster in this benchmark.
+
+|          Method |     Mean |    Error |   StdDev |     Gen 0 |     Gen 1 | Allocated |
+|---------------- |---------:|---------:|---------:|----------:|----------:|----------:|
+| SynchronousLoop | 11.472 s | 0.3365 s | 0.9654 s | 2000.0000 | 1000.0000 |      8 MB |
+|    ParallelLoop |  6.355 s | 0.2787 s | 0.8087 s | 2000.0000 | 1000.0000 |      8 MB |
