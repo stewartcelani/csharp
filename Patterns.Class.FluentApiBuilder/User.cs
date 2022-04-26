b9f84c9f8f3755d9fd84643b46a87bcff23f1a7b
Stewart Celani <me@stewartcelani.com>
2022-04-26 21:24:54 +0930

    Explored how to make a very simple builder class with fluent API like is seen in a lot of .NET libraries.

---
+++ Patterns.Class.FluentApiBuilder/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Patterns.Class.FluentApiBuilder;
+
+public class User {
+    public string Name { get; }
+    public string Email { get; }
+    public int Age { get; }
+
+    public User(string name, string email, int age)
+    {
+        Name = name;
+        Email = email;
+        Age = age;
+    }
+}
---
+++ Patterns.Class.FluentApiBuilder/UserBuilder.cs
@@ -0,0 +1,32 @@
+﻿namespace Patterns.Class.FluentApiBuilder;
+
+public class UserBuilder
+{
+    private string _name;
+    private string _email;
+    private int _age;
+
+    public UserBuilder WithName(string name)
+    {
+        _name = name;
+        return this;
+    }
+
+    public UserBuilder WithEmail(string email)
+    {
+        _email = email;
+        return this;
+    }
+
+    public UserBuilder WithAge(int age)
+    {
+        _age = age;
+        return this;
+    }
+    
+    public User Build()
+    {
+        // A proper implementation would implement error checking here
+        return new User(_name, _email, _age);
+    }
+}
