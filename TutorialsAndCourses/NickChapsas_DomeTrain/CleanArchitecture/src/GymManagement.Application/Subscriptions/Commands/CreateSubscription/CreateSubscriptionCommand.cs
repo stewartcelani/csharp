da1947105228bd8b4af8baa0631797c767027b27
Stewart Celani <me@stewartcelani.com>
2023-10-04 17:08:48 +1030

    56% - Repository & Unit of Work Pattern

--- TutorialsAndCourses/NickChapsas_DomeTrain/CleanArchitecture/src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommand.cs
+++ TutorialsAndCourses/NickChapsas_DomeTrain/CleanArchitecture/src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommand.cs
@@ -1,6 +1,7 @@
 using ErrorOr;
+using GymManagement.Domain.Subscriptions;
 using MediatR;
 
 namespace GymManagement.Application.Subscriptions.Commands.CreateSubscription;
 
-public record CreateSubscriptionCommand(string SubscriptionType, Guid AdminId) : IRequest<ErrorOr<Guid>>;
+public record CreateSubscriptionCommand(string SubscriptionType, Guid AdminId) : IRequest<ErrorOr<Subscription>>;
--- TutorialsAndCourses/NickChapsas_DomeTrain/CleanArchitecture/src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
+++ TutorialsAndCourses/NickChapsas_DomeTrain/CleanArchitecture/src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
@@ -1,12 +1,27 @@
 using ErrorOr;
+using GymManagement.Application.Common.Interfaces;
+using GymManagement.Domain.Subscriptions;
 using MediatR;
 
 namespace GymManagement.Application.Subscriptions.Commands.CreateSubscription;
 
-public class CreateSubscriptionCommandHandler : IRequestHandler<CreateSubscriptionCommand, ErrorOr<Guid>>
+public class CreateSubscriptionCommandHandler : IRequestHandler<CreateSubscriptionCommand, ErrorOr<Subscription>>
 {
-    public async Task<ErrorOr<Guid>> Handle(CreateSubscriptionCommand request, CancellationToken cancellationToken)
+    private readonly ISubscriptionsRepository _subscriptionsRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateSubscriptionCommandHandler(ISubscriptionsRepository subscriptionsRepository, IUnitOfWork unitOfWork)
     {
-        return Guid.NewGuid();
+        _subscriptionsRepository = subscriptionsRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<ErrorOr<Subscription>> Handle(CreateSubscriptionCommand request,
+        CancellationToken cancellationToken)
+    {
+        var subscription = new Subscription { Id = Guid.NewGuid() };
+        await _subscriptionsRepository.AddSubscriptionAsync(subscription);
+        await _unitOfWork.CommitChangesAsync();
+        return subscription;
     }
 }
