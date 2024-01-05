3ca9caf733c5cf39d826ac5ec4f6e7f72447dbf4
Stewart Celani <me@stewartcelani.com>
2024-01-05 13:38:03 +1030

    Benchmarking methods of recursively calculating directory size.

---
+++ Exercises/FolderSize/FolderSize/FolderSize/Benchmarks.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace FolderSize;
+
+[MaxIterationCount(33)]
+public class Benchmarks
+{
+    private const string Path = @"C:\dev";
+    private readonly DirectoryInfo _directoryInfo = new(Path);
+    
+    [Benchmark]
+    public long DirSize()
+    {
+        return _directoryInfo.DirSize();
+    }
+    
+    [Benchmark]
+    public long GetDirectorySize()
+    {
+        return _directoryInfo.GetDirectorySize();
+    }
+
+    [Benchmark]
+    public long GetDirectorySizeParallel()
+    {
+        return _directoryInfo.GetDirectorySizeParallel();
+    }
+}
---
+++ Exercises/FolderSize/FolderSize/FolderSize/DirectoryInfoExtensions.cs
@@ -0,0 +1,60 @@
+﻿namespace FolderSize;
+
+public static class DirectoryInfoExtensions
+{
+    public static long GetDirectorySize(this DirectoryInfo directoryInfo, bool recursive = true)
+    {
+        long startDirectorySize = 0;
+        if (directoryInfo == null || !directoryInfo.Exists)
+            return startDirectorySize; // Return 0 if Directory does not exist.
+
+        // Add size of files in the Current Directory to main size.
+        foreach (var fileInfo in directoryInfo.GetFiles())
+            startDirectorySize += fileInfo.Length;
+
+        // Loop on Subdirectories in the Current Directory and calculate their files' size.
+        if (recursive)
+        {
+            foreach (var subDirectory in directoryInfo.GetDirectories())
+                startDirectorySize += GetDirectorySize(subDirectory, recursive);
+        }
+
+        return startDirectorySize; // Return full Size of this Directory.
+    }
+
+    public static long GetDirectorySizeParallel(this DirectoryInfo directoryInfo, bool recursive = true)
+    {
+        var startDirectorySize = default(long);
+        if (directoryInfo == null || !directoryInfo.Exists)
+            return startDirectorySize; //Return 0 while Directory does not exist.
+
+        //Add size of files in the Current Directory to main size.
+        foreach (var fileInfo in directoryInfo.GetFiles())
+            Interlocked.Add(ref startDirectorySize, fileInfo.Length);
+
+        if (recursive) //Loop on Sub Direcotries in the Current Directory and Calculate it's files size.
+            Parallel.ForEach(directoryInfo.GetDirectories(), subDirectory =>
+                Interlocked.Add(ref startDirectorySize, GetDirectorySizeParallel(subDirectory, recursive)));
+
+        return startDirectorySize; //Return full Size of this Directory.
+    }
+    
+    
+    public static long DirSize(this DirectoryInfo d) 
+    {    
+        long size = 0;    
+        // Add file sizes.
+        FileInfo[] fis = d.GetFiles();
+        foreach (FileInfo fi in fis) 
+        {      
+            size += fi.Length;    
+        }
+        // Add subdirectory sizes.
+        DirectoryInfo[] dis = d.GetDirectories();
+        foreach (DirectoryInfo di in dis) 
+        {
+            size += DirSize(di);   
+        }
+        return size;  
+    }
+}
